17ab3893ee66f4982dced6b9c488e18626c4cb65
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-10-19 12:08:46 -0400

    Initial Commit

---
+++ SwiftUISimpleTabbar/SwiftUISimpleTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISimpleTabbarApp.swift
+//  SwiftUISimpleTabbar
+//
+//  Created by gomathi saminathan on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISimpleTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
